bd564780a93bf202272781d0873ad43e492197fd
Pao <kingpaolofranco60@gmail.com>
2023-06-11 13:33:16 +0800

    change the import of funcs.py
    from: from funcs import *, to: import funcs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from funcs import *
+import funcs
 import telebot
 import os
 
@@ -18,33 +18,41 @@ commands = ['define', 'apod', 'ufacts', 'generateqr', 'help', 'start']
 
 @bot.message_handler(commands=['start'])
 def msg(message):
-    start_msg(message)
+    funcs.start_msg(message)
 
 # HELP
 
 @bot.message_handler(commands=['help'])
 def msg(message):
-    help_msg(message)
+    funcs.help_msg(message)
 
 # NASA ASTRONOMY PICTURE OF THE DAY
 
 @bot.message_handler(commands=['apod'])
 def msg(message):
     msg = bot.send_message(message.chat.id, 'Send a date to find the NASA image of the day. YYYY-MM-DD format.')
-    bot.register_next_step_handler(msg, apod)
+    bot.register_next_step_handler(msg, funcs.apod)
 
 # USELESS FACTS
 
 @bot.message_handler(commands=['ufacts'])
 def msg(message):
-    ufacts(message)
+    funcs.ufacts(message)
 
 # GENERATE QR CODE
 
 @bot.message_handler(commands=['generateqr'])
 def msg(message):
     msg = bot.send_message(message.chat.id, 'Send a text to generate qr code.')
-    bot.register_next_step_handler(msg, generateqr)
+    bot.register_next_step_handler(msg, funcs.generateqr)
+
+# DEFINE
+
+@bot.message_handler(commands=['define'])
+def msg(message):
+    msg = bot.send_message(message.chat.id, 'Send a word to define')
+    bot.register_next_step_handler(msg, funcs.define)
+
 
 bot.enable_save_next_step_handlers(delay=2)
 bot.load_next_step_handlers()
